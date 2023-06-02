3a6994b09b5d969f1213f9873c60cb3a1f7b1f3b
Savan Vekariya <savan.vekariya@sap.com>
2023-06-02 21:48:05 +0200

    added a comment

--- insertionSort.js
+++ insertionSort.js
@@ -1,7 +1,7 @@
 function insertionSort(arr){
 for(let [i, element] of arr.entries()){
     let j = i-1;
-    while(j>=0 && arr[j]>element){
+    while(j>=0 && arr[j]>element){ // while(j>=0 && arr[j+1]<arr[j])
         let temp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = temp;
