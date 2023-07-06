4b9384b0f636f7d30b5374b81ee962a654202059
Savan Vekariya <savanvekariya.sv@gmail.com>
2023-07-06 18:02:23 +0200

    other methods to reverse string

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,25 @@
-function reverseString(inputString){
+function reverseString(inputString) {
     return inputString.split('').reverse().join('')
 }
 
 console.log(reverseString('Hello'))
+
+function reverseString2(inputString) {
+    reverseString = ''
+    inputString.split('').forEach(element => {
+        reverseString = element + reverseString
+    });
+    console.log(reverseString)
+}
+
+reverseString2('hello')
+
+
+function reverseString3(inputString) {
+    return inputString.split('').reduce((prev, curr) => {
+        // console.log('prev-> ', prev)
+        // console.log('curr-> ', curr)
+        return prev + curr
+    })
+}
+console.log(reverseString3('hello'))
