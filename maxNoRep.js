f5f768efdcc8dc4aef0ffdca21b101653e3fc544
savanvekariya <savanvekariya.sv@gmail.com>
2023-12-02 15:01:51 +0100

    avrage

--- maxNoRep.js
+++ maxNoRep.js
@@ -9,4 +9,13 @@ function maxNo(numArr) {
   console.log(Math.max(...Object.values(numObj)));
 }
 
-maxNo([1, 2, 2, 3, 1, 5, 1, 8, 1]);
+// maxNo([1, 2, 2, 3, 1, 5, 1, 8, 1]);
+
+function avrage(...c) {
+  let sum = c.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+  console.log(sum / c.length);
+}
+
+avrage(1, 1, 3, 5, 6, 7, 8, 9);
